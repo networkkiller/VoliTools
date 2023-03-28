16e51355e6e908bc813c34df0319aeaa0487e780
networkkiller <71563446+networkkiller@users.noreply.github.com>
2023-03-28 10:18:52 -0400

    Update VoliTools.py

--- VoliTools.py
+++ VoliTools.py
@@ -14,13 +14,26 @@ opciones = {
                 'comando': 'git clone https://github.com/networkkiller/PainFile.git',
             },
             '2': {
-                'texto': f'{Fore.GREEN}Keyloger{Style.RESET_ALL}',
-                'comando': 'git clone https://github.com/repo1_2.git',
+                'texto': f'{Fore.GREEN}ip-keyloger(need to allow lesssecureapps ){Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/shiky8/ip-keyloger.git',
             },
             '3': {
                 'texto': f'{Fore.GREEN}MalwareSourceCode{Style.RESET_ALL}',
                 'comando': 'git clone https://github.com/vxunderground/MalwareSourceCode.git',
             },
+            '4': {
+                'texto': f'{Fore.GREEN}AdWare{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/SKocur/Simple-Adware.git',
+            },
+             '5': {
+                'texto': f'{Fore.GREEN}RAMtester{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/SKocur/Malware-Collection/blob/master/h3wroMemoryLeaker.c',
+            },
+            '6': {
+                'texto': f'{Fore.GREEN}RemoteClient{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/SKocur/Malware-Collection/blob/master/h3wroRemoterClient.cpp',
+            },
+            
         }
     },
     '2': {
@@ -34,6 +47,14 @@ opciones = {
                 'texto': f'{Fore.YELLOW}AndroidRat{Style.RESET_ALL}',
                 'comando': 'git clone https://github.com/repo2_2.git',
             },
+            '3':{
+                'texto': f'{Fore.YELLOW}XploitSPY{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/XploitWizer-Community/XploitSPY.git'
+            },
+            '4':{
+                'texto': f'{Fore.YELLOW}ADB-Xploit(usaras una dork en shodan2){Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/henry-richard7/ADB-Xploit.git'
+            }
         }
     },
     '3': {
@@ -92,8 +113,114 @@ opciones = {
                 'texto': f'{Fore.CYAN}Git-Scanner{Style.RESET_ALL}',
                 'comando': 'git clone https://github.com/HightechSec/git-scanner.git',
             },
+             '11': {
+                'texto': f'{Fore.CYAN}JwrXploiter{Style.RESET_ALL}',
+                'comando': 'https://github.com/DontPanicO/jwtXploiter.git',
+            },
         }
         
+    },
+    '5': {
+        'texto': f'{Fore.BLUE}Cibersecurity{Style.RESET_ALL}',
+        'opciones_secundarias': {
+            '1': {
+                'texto': f'{Fore.BLUE}hijackthis{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/dragokas/hijackthis.git',
+            },
+            '2': {
+                'texto': f'{Fore.BLUE}apache-ultimate-bad-bot-blocker{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/mitchellkrogza/apache-ultimate-bad-bot-blocker.git',
+            },
+             '3': {
+                'texto': f'{Fore.BLUE}SecurityXploit{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/PhHitachi/SecurityXploit.git',
+            },
+             '4': {
+                'texto': f'{Fore.BLUE}FazScan{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/mfazrinizar/FazScan.git',
+            },
+            '5': {
+                'texto': f'{Fore.BLUE}Faz-SHC (encriptar el texto que le das a un Shellcode){Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/mfazrinizar/Faz-SHC.git',
+            },
+        }
+    },
+    '6': {
+        'texto': f'{Fore.BLUE}Anonimato{Style.RESET_ALL}',
+        'opciones_secundarias': {
+            '1': {
+                'texto': f'{Fore.BLUE}Whoami{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/owerdogan/whoami-project.git',
+            },
+            '2': {
+                'texto': f'{Fore.BLUE}apache-ultimate-bad-bot-blocker{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/mitchellkrogza/apache-ultimate-bad-bot-blocker.git',
+            },
+        }
+    },
+    '7': {
+        'texto': f'{Fore.BLUE}Obfuscacion{Style.RESET_ALL}',
+        'opciones_secundarias': {
+            '1': {
+                'texto': f'{Fore.BLUE}javascript-obfuscator{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/javascript-obfuscator/javascript-obfuscator.git',
+            },
+            '2': {
+                'texto': f'{Fore.BLUE}python-obfuscator{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/davidteather/python-obfuscator.git',
+            },
+        }
+    },
+    '8': {
+        'texto': f'{Fore.BLUE}Explotacion{Style.RESET_ALL}',
+        'opciones_secundarias': {
+            '1': {
+                'texto': f'{Fore.BLUE}Windows_Postex(Leer y editar, no funcionara sino lo editan ){Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/A-mIn3/Windows_Postex.git',
+            },
+            '2': {
+                'texto': f'{Fore.BLUE}gobiovuls(2016-2022){Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/gobysec/GobyVuls.git',
+            },
+            
+            '3': {
+                'texto': f'{Fore.BLUE}ROPgadget{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/JonathanSalwan/ROPgadget.git',
+            },
+             '4': {
+                'texto': f'{Fore.BLUE}Binary-Explotation{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/KazakosVas/Binary-Explotation.git',
+            },
+             '5': {
+                'texto': f'{Fore.BLUE}Xploit{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/shargon/Xploit.git',
+            },
+             '6': {
+                'texto': f'{Fore.BLUE}AutoXploit{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/Yashvendra/AutoXploit.git',
+            },
+            '7': {
+                'texto': f'{Fore.BLUE}VertXploit(solo para control de acceso HID VertX y EDGE){Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/coldfusion39/VertXploit.git',
+            },
+            '8': {
+                'texto': f'{Fore.BLUE}0Day-Xploit{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/maschil/0day-Xploit.git',
+            },
+             '9': {
+                'texto': f'{Fore.BLUE}FollinaXploit(en caso de encontrar un objetivo sin parcharla aun){Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/0xAbbarhSF/FollinaXploit.git',
+            },
+            '10': {
+                'texto': f'{Fore.BLUE}FazPort{Style.RESET_ALL}',
+                'comando': 'https://github.com/mfazrinizar/FazPort.git',
+            },
+             '11': {
+                'texto': f'{Fore.BLUE}IndoXploit-Shell{Style.RESET_ALL}',
+                'comando': 'https://github.com/flux10n/IndoXploit-Shell.git',
+            },
+           
+        }
     },
 }
 
