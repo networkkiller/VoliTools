742ae7c502a0a97e6ebf22a3b63c84081e1d135e
networkkiller <71563446+networkkiller@users.noreply.github.com>
2023-03-29 07:56:49 -0400

    Update VoliTools.py

--- VoliTools.py
+++ VoliTools.py
@@ -34,6 +34,7 @@ opciones = {
                 'comando': 'git clone https://github.com/SKocur/Malware-Collection/blob/master/h3wroRemoterClient.cpp',
             },
             
+            
         }
     },
     '2': {
@@ -115,7 +116,23 @@ opciones = {
             },
              '11': {
                 'texto': f'{Fore.CYAN}JwrXploiter{Style.RESET_ALL}',
-                'comando': 'https://github.com/DontPanicO/jwtXploiter.git',
+                'comando': 'git clone https://github.com/DontPanicO/jwtXploiter.git',
+            },
+             '11': {
+                'texto': f'{Fore.CYAN}ToxSin{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/t3l3machus/toxssin.git',
+            },
+             '12': {
+                'texto': f'{Fore.CYAN}Xless{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/r3curs1v3-pr0xy/xless.git',
+            },
+            '13': {
+                'texto': f'{Fore.CYAN}Xsshunter-Express(Uso de Docker){Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/r3curs1v3-pr0xy/xsshunter-express.git',
+            },
+            '14': {
+                'texto': f'{Fore.CYAN}Sub404{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/r3curs1v3-pr0xy/sub404.git',
             },
         }
         
@@ -143,6 +160,47 @@ opciones = {
                 'texto': f'{Fore.BLUE}Faz-SHC (encriptar el texto que le das a un Shellcode){Style.RESET_ALL}',
                 'comando': 'git clone https://github.com/mfazrinizar/Faz-SHC.git',
             },
+             '6': {
+                'texto': f'{Fore.BLUE}ssh-log-alerta{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/t3l3machus/ssh-log-alert.git',
+            },
+            '7': {
+                'texto': f'{Fore.RED}BruteMySQL{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/S12cybersecurity/BruteMySQL.git',
+            },
+             '8': {
+                'texto': f'{Fore.CYAN}Sub404{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/r3curs1v3-pr0xy/sub404.git',
+            },
+            '9': {
+                'texto': f'{Fore.CYAN}Commix{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/commixproject/commix.git',
+            },
+            '10': {
+                'texto': f'{Fore.CYAN}Dploot{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/zblurx/dploot.git',
+            },
+            '11': {
+                'texto': f'{Fore.CYAN}ServiceDetector{Style.RESET_ALL}',
+                'comando': ' git clone https://github.com/tothi/serviceDetector.git',
+            },
+             '12': {
+                'texto': f'{Fore.CYAN}PdfSpeaker{Style.RESET_ALL}',
+                'comando': ' git clone https://github.com/Toxic-Noob/PdfSpeaker.git',
+            },
+            '13': {
+                'texto': f'{Fore.CYAN}ProcList{Style.RESET_ALL}',
+                'comando': ' git clone https://github.com/S12cybersecurity/ProcList.git',
+            },
+             '14': {
+                'texto': f'{Fore.CYAN}Hopper-Scripts{Style.RESET_ALL}',
+                'comando': ' git clone https://github.com/droe/hopper-scripts.git',
+            },
+             '14': {
+                'texto': f'{Fore.CYAN}Toolies{Style.RESET_ALL}',
+                'comando': ' git clonehttps://github.com/expl0itabl3/Toolies',
+            },
+            
         }
     },
     '6': {
@@ -169,55 +227,136 @@ opciones = {
                 'texto': f'{Fore.BLUE}python-obfuscator{Style.RESET_ALL}',
                 'comando': 'git clone https://github.com/davidteather/python-obfuscator.git',
             },
+             '3': {
+                'texto': f'{Fore.BLUE}Emcc-obf(Esta basdo en LLVM y Docker){Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/HakonHarnes/emcc-obf.git',
+            },
+            '4': {
+                'texto': f'{Fore.BLUE}Chameleon{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/klezVirus/chameleon.git',
+            },
+            '5': {
+                'texto': f'{Fore.BLUE}Boobsnail{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/STMCyber/boobsnail.git',
+            },
         }
     },
     '8': {
-        'texto': f'{Fore.BLUE}Explotacion{Style.RESET_ALL}',
+        'texto': f'{Fore.RED}REDTEAMING{Style.RESET_ALL}',
         'opciones_secundarias': {
             '1': {
-                'texto': f'{Fore.BLUE}Windows_Postex(Leer y editar, no funcionara sino lo editan ){Style.RESET_ALL}',
+                'texto': f'{Fore.RED}Windows_Postex(Leer y editar, no funcionara sino lo editan ){Style.RESET_ALL}',
                 'comando': 'git clone https://github.com/A-mIn3/Windows_Postex.git',
             },
             '2': {
-                'texto': f'{Fore.BLUE}gobiovuls(2016-2022){Style.RESET_ALL}',
+                'texto': f'{Fore.RED}gobiovuls(2016-2022){Style.RESET_ALL}',
                 'comando': 'git clone https://github.com/gobysec/GobyVuls.git',
             },
             
             '3': {
-                'texto': f'{Fore.BLUE}ROPgadget{Style.RESET_ALL}',
+                'texto': f'{Fore.RED}ROPgadget (solo admite arquitecturas: x86, x64, ARM, ARM64, PowerPC, SPARC, MIPS){Style.RESET_ALL}',
                 'comando': 'git clone https://github.com/JonathanSalwan/ROPgadget.git',
             },
              '4': {
-                'texto': f'{Fore.BLUE}Binary-Explotation{Style.RESET_ALL}',
+                'texto': f'{Fore.RED}Binary-Explotation{Style.RESET_ALL}',
                 'comando': 'git clone https://github.com/KazakosVas/Binary-Explotation.git',
             },
              '5': {
-                'texto': f'{Fore.BLUE}Xploit{Style.RESET_ALL}',
+                'texto': f'{Fore.RED}Xploit{Style.RESET_ALL}',
                 'comando': 'git clone https://github.com/shargon/Xploit.git',
             },
              '6': {
-                'texto': f'{Fore.BLUE}AutoXploit{Style.RESET_ALL}',
+                'texto': f'{Fore.RED}AutoXploit{Style.RESET_ALL}',
                 'comando': 'git clone https://github.com/Yashvendra/AutoXploit.git',
             },
             '7': {
-                'texto': f'{Fore.BLUE}VertXploit(solo para control de acceso HID VertX y EDGE){Style.RESET_ALL}',
+                'texto': f'{Fore.RED}VertXploit(solo para control de acceso HID VertX y EDGE){Style.RESET_ALL}',
                 'comando': 'git clone https://github.com/coldfusion39/VertXploit.git',
             },
             '8': {
-                'texto': f'{Fore.BLUE}0Day-Xploit{Style.RESET_ALL}',
+                'texto': f'{Fore.RED}0Day-Xploit{Style.RESET_ALL}',
                 'comando': 'git clone https://github.com/maschil/0day-Xploit.git',
             },
              '9': {
-                'texto': f'{Fore.BLUE}FollinaXploit(en caso de encontrar un objetivo sin parcharla aun){Style.RESET_ALL}',
+                'texto': f'{Fore.RED}FollinaXploit(en caso de encontrar un objetivo sin parcharla aun){Style.RESET_ALL}',
                 'comando': 'git clone https://github.com/0xAbbarhSF/FollinaXploit.git',
             },
             '10': {
-                'texto': f'{Fore.BLUE}FazPort{Style.RESET_ALL}',
-                'comando': 'https://github.com/mfazrinizar/FazPort.git',
+                'texto': f'{Fore.RED}FazPort{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/mfazrinizar/FazPort.git',
             },
-             '11': {
-                'texto': f'{Fore.BLUE}IndoXploit-Shell{Style.RESET_ALL}',
-                'comando': 'https://github.com/flux10n/IndoXploit-Shell.git',
+             
+             '12': {
+                'texto': f'{Fore.RED}ADX{Style.RESET_ALL}',
+                'comando': ' git clone https://github.com/djunekz/axd.git',
+            },
+            '13': {
+                'texto': f'{Fore.RED}Villain (Hacer bypass al codeshell, preguntar por el bypass){Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/t3l3machus/Villain.git',
+            },
+            '14': {
+                'texto': f'{Fore.RED}Hoaxshell{Style.RESET_ALL}',
+                'comando': ' git clone https://github.com/t3l3machus/hoaxshell.git',
+            },
+            '15': {
+                'texto': f'{Fore.RED}WWWtree{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/t3l3machus/wwwtree.git',
+            },
+            '16': {
+                'texto': f'{Fore.RED}PoC for CVE-2023-22960 :) denada{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/t3l3machus/CVE-2023-22960.git',
+            },
+            '17': {
+                'texto': f'{Fore.RED}BabelStrike{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/t3l3machus/BabelStrike.git',
+            },
+            '18': {
+                'texto': f'{Fore.RED}FileExtrator{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/a7t0fwa7/FileExtractor.git',
+            },
+            '19': {
+                'texto': f'{Fore.RED}RedNeuron{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/S12cybersecurity/RedNeuron.git',
+            },
+            '20': {
+                'texto': f'{Fore.RED}Chaos-Rootkit{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/ZeroMemoryEx/Chaos-Rootkit.git',
+            },
+             '21': {
+                'texto': f'{Fore.RED}WCE{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/Cyber-Guy1/WCE.git',
+            },
+             '21': {
+                'texto': f'{Fore.RED}RatInject{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/5l1v3r1/RatInject.git',
+            },
+            '22': {
+                'texto': f'{Fore.RED}Windows_LPE_AFD_CVE-2023-21768 (Elevacion de privilegios de procesos(PID) ){Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/chompie1337/Windows_LPE_AFD_CVE-2023-21768.git',
+            },
+            '23': {
+                'texto': f'{Fore.RED}Black-Angel-Rootkit{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/XaFF-XaFF/Black-Angel-Rootkit.git',
+            },
+            '23': {
+                'texto': f'{Fore.RED}BruteMySQL{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/S12cybersecurity/BruteMySQL.git',
+            },
+             '24': {
+                'texto': f'{Fore.RED}ExploitDB{Style.RESET_ALL}',
+                'comando': 'git clone https://gitlab.com/exploit-database/exploitdb.git',
+            },
+            '25': {
+                'texto': f'{Fore.RED}Boobsnail{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/STMCyber/boobsnail.git',
+            },
+            '26': {
+                'texto': f'{Fore.RED}nosferatu (no funciona en AD){Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/kindtime/nosferatu.git',
+            },
+             '26': {
+                'texto': f'{Fore.RED}persistencia en linux and servers{Style.RESET_ALL}',
+                'comando': 'git clone https://github.com/Harsh-bash/Linux_Persistence_Techniques.git',
             },
            
         }
